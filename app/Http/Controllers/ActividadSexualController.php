743fee6fc8121e745159c460508186440f3be720
bagtrejo <braslytrejo@gmail.com>
2019-10-04 16:54:55 -0600

    Create ActividadSexualController.php

---
+++ app/Http/Controllers/ActividadSexualController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ActividadSexual;
+use Illuminate\Http\Request;
+
+class ActividadSexualController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $actividades_sexuales = ActividadSexual::get();
+        echo json_encode($actividades_sexuales);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $actividad_sexual = new ActividadSexual();
+        $actividad_sexual->actividad_sexual = $request->input(['actividad_sexual']);
+        $actividad_sexual->edad_inicio_sexual = $request->input(['edad_inicio_sexual']);
+        $actividad_sexual->numero_parejas_sexuales = $request->input(['numero_parejas_sexuales']);
+        $actividad_sexual->practicas_sexuales_riesgo = $request->input(['practicas_sexuales_riesgo']);
+        $actividad_sexual->id_paciente = $request->input(['id_paciente']);
+
+        $actividad_sexual->save();
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ActividadSexual  $actividadSexual
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActividadSexual $actividadSexual)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ActividadSexual  $actividadSexual
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActividadSexual $actividadSexual)
+    {
+        //
+    }
+}
