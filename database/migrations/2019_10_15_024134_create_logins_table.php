b239178902d27120e41bb6e5a48529c97246f6ef
bagtrejo <braslytrejo@gmail.com>
2019-10-14 21:29:46 -0600

    Create 2019_10_15_024134_create_logins_table.php

---
+++ database/migrations/2019_10_15_024134_create_logins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->bigIncrements('id_login');
+            $table->string('cuenta');
+            $table->string('nombre');
+            $table->string('carrera');
+            $table->string('centro');
+            $table->string('indice_global');
+            $table->string('indice_periodo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logins');
+    }
+}
