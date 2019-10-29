bdc4b39361c741c5e37ab01bcfc17192297c3b01
sevillalberto <sevillajose.as@gmail.com>
2019-10-28 21:50:38 -0600

    Update InventarioController.php

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -28,8 +28,10 @@ class InventarioController extends Controller
     public function store(Request $request)
     {
         $inventario = new Inventario(); 
-        $inventario->nombre = $request->input('nombre');
         $inventario->cantidad = $request->input('cantidad');
+        $inventario->nombre = $request->input('nombre');
+        $inventario->descripcion = $request->input('descripcion');
+        $inventario->fecha_vencimiento = $request->input('fecha_vencimiento');
         $inventario->save();
 
         echo json_encode($inventario);
@@ -46,8 +48,10 @@ class InventarioController extends Controller
     public function update(Request $request, $inventario_id)
     {
         $inventario = Inventario::find($inventario_id); 
-        $inventario->nombre = $request->input('nombre');
         $inventario->cantidad = $request->input('cantidad');
+        $inventario->nombre = $request->input('nombre');
+        $inventario->descripcion = $request->input('descripcion');
+        $inventario->fecha_vencimiento = $request->input('fecha_vencimiento');
         $inventario->save();
 
         echo json_encode($inventario);
