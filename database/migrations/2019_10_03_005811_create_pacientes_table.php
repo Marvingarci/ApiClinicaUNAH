e21468dced75295ddf5eec4e6b2b46cf07cd295f
Marvingarci <eulalio17@yahoo.es>
2019-10-30 10:21:45 -0600

    Update 2019_10_03_005811_create_pacientes_table.php

--- database/migrations/2019_10_03_005811_create_pacientes_table.php
+++ database/migrations/2019_10_03_005811_create_pacientes_table.php
@@ -36,7 +36,7 @@ class CreatePacientesTable extends Migration
             $table->string('temperatura')->nullable();
             $table->string('presion')->nullable();
             $table->string('pulso')->nullable();
-            $table->char('categoria')->default('E');
+            $table->char('categoria');
             $table->timestamps();
 
             $table->primary('id_paciente');
