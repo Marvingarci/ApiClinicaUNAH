f7110d59b71d4093bb86e0c3b8177d290e96d183
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-02 18:02:51 -0600

    Update 2019_10_13_162802_create_login_admins_table.php

--- database/migrations/2019_10_13_162802_create_login_admins_table.php
+++ database/migrations/2019_10_13_162802_create_login_admins_table.php
@@ -14,7 +14,7 @@ class CreateLoginAdminsTable extends Migration
     public function up()
     {
         Schema::create('login_admins', function (Blueprint $table) {
-            $table->bigIncrements('id_loginAdmin');
+            $table->bigIncrements('id');
             $table->string('usuario_admin');
             $table->string('contrasenia_admin');
             $table->string('nombre_admin');
