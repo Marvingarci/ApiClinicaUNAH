c47af76dc56bed33192e84f598c630bc6eafd5a1
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-08 23:16:18 -0600

    Create 2019_11_09_021620_create_medicos_table.php

---
+++ database/migrations/2019_11_09_021620_create_medicos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('usuarioM');
+            $table->string('contraseniaM');
+            $table->string('nombreM');
+            $table->string('identidadM');
+            $table->string('especialidadM');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
