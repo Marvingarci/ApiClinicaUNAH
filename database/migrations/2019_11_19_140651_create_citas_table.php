d4000913ea0b2a67604c35ea05ad120d0fae73ba
Marvingarci <eulalio17@yahoo.es>
2019-11-19 13:44:18 -0600

    Create 2019_11_19_140651_create_citas_table.php

---
+++ database/migrations/2019_11_19_140651_create_citas_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->char('id_paciente');
+            $table->string('peso')->nullable();
+            $table->string('talla')->nullable();
+            $table->string('imc')->nullable();
+            $table->string('temperatura')->nullable();
+            $table->string('presion')->nullable();
+            $table->string('pulso')->nullable();
+            $table->string('siguiente_cita')->nullable();
+            $table->string('observaciones')->nullable();
+            $table->string('impresion')->nullable();
+            $table->string('indicaciones')->nullable();
+            $table->integer('Remitido')->nullable();
+            $table->string('fechayHora')->nullable();
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+}
