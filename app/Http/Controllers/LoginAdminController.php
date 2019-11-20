e98e14719f9712895a75ed26b6b3c03df80941ae
sevillalberto <sevillajose.as@gmail.com>
2019-11-20 10:59:54 -0600

    Update LoginAdminController.php

--- app/Http/Controllers/LoginAdminController.php
+++ app/Http/Controllers/LoginAdminController.php
@@ -14,6 +14,7 @@ class LoginAdminController extends Controller
      */
     public function index()
     {
+
         $login_admin = LoginAdmin::get();
         echo json_encode($login_admin);
     }
