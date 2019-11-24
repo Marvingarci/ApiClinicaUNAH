81d7a04da2e1820c904def436346f80a25124d3f
bagtrejo <braslytrejo@gmail.com>
2019-11-24 14:48:22 -0600

    Create 2019_11_24_203230_funcion_insertar_antecedente.php

---
+++ database/migrations/2019_11_24_203230_funcion_insertar_antecedente.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FuncionInsertarAntecedente extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // $available = Constant::PRODUCT_AVAILABLE_TRUE | Constant::PRODUCT_AVAILABLE_STOCK_TRUE;
+
+        $sql = 
+
+        'DROP FUNCTION IF EXISTS UNAVAILABLE_PRODS;
+        CREATE FUNCTION insertar_antecedente(a varchar(50)) returns int
+            deterministic
+        begin 
+            declare id int;
+        
+            IF(a not in (select antecedente from antecedentes)) then
+                insert into antecedentes(antecedente) values (a);		
+            END IF;
+            
+            return (select id_antecedente from antecedentes where antecedente = a);
+        
+            
+        END';
+
+        DB::unprepared($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // esto no da !!
+        DB::unprepared('drop function if exits insertar_antecedente');
+    }
+}
