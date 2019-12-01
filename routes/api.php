9d6111e206a18272e1ce58080eb4807be09651d8
Marvingarci <eulalio17@yahoo.es>
2019-12-01 09:29:06 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::resource('medicos','MedicosController');
 //rutas alberto
 Route::resource('inventarios','InventarioController');
 Route::get('medicamentos','InventarioController@obtenerMedicamentos');
+Route::post('medicamentos/egreso','InventarioController@disminucion');
 
 
 
