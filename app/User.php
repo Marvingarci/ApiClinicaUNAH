e9b6a727cb4fe4264dccb2b978cfccae0b643355
bagtrejo <braslytrejo@gmail.com>
2020-02-04 20:25:18 -0600

    Update User.php

--- app/User.php
+++ app/User.php
@@ -1,39 +1,33 @@
 <?php
+namespace  App;
 
-namespace App;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User  as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class  User  extends  Authenticatable  implements  JWTSubject {
+	use  Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $table = 'logins'; // colocamos el nombre de la tabla
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	protected  $fillable = [
+		'cuenta', 'password', 'nombre', 'carrera','centro','numero_identidad','imagen'
+	];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	protected  $hidden = [
+		'password', 'remember_token',
+	];
+
+	protected  $casts = [
+		'email_verified_at' => 'datetime',
+	];
+
+	public  function  getJWTIdentifier() {
+		return  $this->getKey();
+	}
+
+	public  function  getJWTCustomClaims() {
+		return [];
+	}
 }
