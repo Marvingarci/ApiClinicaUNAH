aadc35241bc4e7a696f3cfe15a4c68025cb6e827
bagtrejo <braslytrejo@gmail.com>
2020-02-20 09:28:03 -0600

    Update User.php

--- app/User.php
+++ app/User.php
@@ -9,7 +9,8 @@ use Illuminate\Foundation\Auth\User  as Authenticatable;
 class  User  extends  Authenticatable  implements  JWTSubject {
 	use  Notifiable;
 
-    protected $table = 'logins'; // colocamos el nombre de la tabla
+	protected $table = 'logins'; // colocamos el nombre de la tabla
+	protected $primaryKey = 'id_login';
 
 	protected  $fillable = [
 		'id_login','cuenta', 'password', 'nombre', 'carrera','centro','numero_identidad','imagen'
