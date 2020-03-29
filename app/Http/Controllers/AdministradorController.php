ae66c9df143292845c809c60b5fe2aa0ad68b0fb
bagtrejo <braslytrejo@gmail.com>
2020-03-28 19:02:19 -0600

    Update AdministradorController.php

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Administrador;
 use Illuminate\Http\Request;
-use Illuminate\Support\facades\DB;
+use DB;
 use Illuminate\Support\Facades\Hash;
 
 class AdministradorController extends Controller
