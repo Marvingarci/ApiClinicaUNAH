31fb6e7b4537791565f94dd2716f1856130554d4
bagtrejo <braslytrejo@gmail.com>
2020-03-28 19:03:20 -0600

    Update TelefonosPacientesController.php

--- app/Http/Controllers/TelefonosPacientesController.php
+++ app/Http/Controllers/TelefonosPacientesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\TelefonosPacientes;
-use Illuminate\Support\facades\DB;
+use DB;
 use Illuminate\Http\Request;
 
 class TelefonosPacientesController extends Controller
