6d29dbbd0833e43f186b8073c0fc7d7e2067708e
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-04-02 08:15:03 -0600

    Update CitasController.php

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -101,63 +101,30 @@ class CitasController extends Controller
 
             $cita = DB::table('citas')
             ->join('remitidoa', 'citas.remitido', '=', 'remitidoa.id_seccion')
-            //->join('categorias', 'pacientes.categoria', '=', 'categorias.id_categorias')
             ->where('id_paciente', $id_paciente)
             ->select(
-                'id_paciente','peso', 'talla','imc',
-                'temperatura', 'presion', 'pulso', 'siguiente_cita',
-                'observaciones', 'impresion', 'indicaciones', 'remitidoa.seccion', 'fechayHora'
-                )
-                
+            'id_paciente','peso', 'talla','imc',
+            'temperatura', 'presion', 'pulso', 'siguiente_cita',
+            'observaciones', 'impresion', 'indicaciones', 'remitidoa.seccion', 'fechayHora'
+            )
             ->get();
             return response()->json([$cita]);
 
-            
-
-        }else{
-        
+        }else{        
             $cita = DB::table('citas')
             ->join('remitidoa', 'citas.remitido', '=', 'remitidoa.id_seccion')
             ->join('inventarios', 'citas.nombre', '=', 'inventarios.id_inventario')
             //->join('categorias', 'pacientes.categoria', '=', 'categorias.id_categorias')
             ->where('id_paciente', $id_paciente)
             ->select(
-                'id_paciente','peso', 'talla','imc',
-                'temperatura', 'presion', 'pulso', 'siguiente_cita',
-                'observaciones', 'impresion','inventarios.nombre', 'indicaciones', 'remitidoa.seccion', 'fechayHora'
-                )
-                
+            'id_paciente','peso', 'talla','imc',
+            'temperatura', 'presion', 'pulso', 'siguiente_cita',
+            'observaciones', 'impresion','inventarios.nombre', 'indicaciones', 'remitidoa.seccion', 'fechayHora'
+            )                
             ->get();
             return response()->json([$cita]);
-
         }
-
-       
-
-
-        // if(empty($paciente)){
-
-
-        //     $paciente = DB::table('citas')
-        //     ->join('remitidoa', 'citas.remitido', '=', 'remitidoa.id_seccion')
-        //     ->where('id_paciente', $id_paciente)
-        //     ->select(
-        //         'id_paciente','peso', 'talla','imc',
-        //         'temperatura', 'presion', 'pulso', 'siguiente_cita',
-        //         'observaciones', 'impresion', 'nombre', 'indicaciones', 'remitidoa.seccion', 'fechayHora'
-        //         )
-                
-        //     ->get();
-
-        //     return response()->json($paciente);
-
-        // }else{
-
-        //     return response()->json($paciente);
-        // }
-
-
-    }
+     }
 
     /**
      * Show the form for editing the specified resource.
