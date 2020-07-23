b276af775bd2819bbb2f74849c8437ea1b0da027
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-07-23 16:29:55 -0600

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,5 +1,13 @@
-<h2>Clinica UNAH-TEC</h2>
-<a href="{{ $link }}">{{ $link }}</a>
-<h5>Ingrese a este link, para que cambies tu contraseña</h5>
-</a>
+<p> Hola, Estimado/a</p>
+
+<p> Usted solicitó un restablecimiento de contraseña para su cuenta en Clínica UNAH-TEC Danlí </p>
+
+<p> Para confirmar esta petición, y establecer una nueva contraseña para su cuenta, </p>
+
+<p> por favor vaya a la siguiente dirección de internet: <a href="{{ $link }}">cambio de contraseña UNAH-TEC Danlí</a></p>
+
+<p> Si usted no ha solicitado este reestablecimiento de contraseña, no necesita realizar ninguna acción </p>
+
+
+
 
